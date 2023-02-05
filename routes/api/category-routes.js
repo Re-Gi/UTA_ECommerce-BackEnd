fd32f4fa1aa9be5fddb70740115ae8b722294d82
Rebecca Girndt <r.l.girndt@gmail.com>
2023-02-05 14:14:46 -0600

    able to update category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,8 +37,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
